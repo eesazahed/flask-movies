d3b3055c432b2c77052b5a4906754c1a37aeba33
Eesa Zahed <eesa.zahed@gmail.com>
2025-07-22 01:59:10 -0500

    add app_context

--- app.py
+++ app.py
@@ -21,66 +21,59 @@ def init_db():
     ''')
 
 
-@app.before_first_request
-def initialize():
+with app.app_context():
     init_db()
 
+    @app.errorhandler(404)
+    def handle_404(e):
+        return render_template('custom404.html')
 
-@app.errorhandler(404)
-def handle_404(e):
-    return render_template('custom404.html')
+    @app.route('/')
+    def index():
+        movies = db.execute('SELECT * FROM movies')
+        return render_template('index.html', movies=movies)
 
+    @app.route('/add', methods=['GET', 'POST'])
+    def add():
+        if request.method == 'POST':
+            title = request.form['title']
+            year = request.form['year']
+            genre = request.form['genre']
+            db.execute(
+                'INSERT INTO movies (title, year, genre) VALUES (?, ?, ?)', title, year, genre)
+            return redirect(url_for('index'))
+        return render_template('add.html')
 
-@app.route('/')
-def index():
-    movies = db.execute('SELECT * FROM movies')
-    return render_template('index.html', movies=movies)
+    @app.route('/edit/<int:id>', methods=['GET', 'POST'])
+    def edit(id):
+        movie = db.execute('SELECT * FROM movies WHERE id = ?', id)
+        if not movie:
+            return redirect(url_for('index'))
+        movie = movie[0]
 
+        if request.method == 'POST':
+            title = request.form['title']
+            year = request.form['year']
+            genre = request.form['genre']
+            db.execute(
+                'UPDATE movies SET title = ?, year = ?, genre = ? WHERE id = ?', title, year, genre, id)
+            return redirect(url_for('index'))
+        return render_template('edit.html', movie=movie)
 
-@app.route('/add', methods=['GET', 'POST'])
-def add():
-    if request.method == 'POST':
-        title = request.form['title']
-        year = request.form['year']
-        genre = request.form['genre']
-        db.execute(
-            'INSERT INTO movies (title, year, genre) VALUES (?, ?, ?)', title, year, genre)
+    @app.route('/delete/<int:id>')
+    def delete(id):
+        db.execute('DELETE FROM movies WHERE id = ?', id)
         return redirect(url_for('index'))
-    return render_template('add.html')
 
-
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
-    movie = db.execute('SELECT * FROM movies WHERE id = ?', id)
-    if not movie:
-        return redirect(url_for('index'))
-    movie = movie[0]
-
-    if request.method == 'POST':
-        title = request.form['title']
-        year = request.form['year']
-        genre = request.form['genre']
-        db.execute(
-            'UPDATE movies SET title = ?, year = ?, genre = ? WHERE id = ?', title, year, genre, id)
-        return redirect(url_for('index'))
-    return render_template('edit.html', movie=movie)
-
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    db.execute('DELETE FROM movies WHERE id = ?', id)
-    return redirect(url_for('index'))
-
-
-@app.route('/commit')
-def commit():
-    output = os.popen('git log -1 --pretty=format:"%h|%s|%cr"').read()
-    commit_hash, description, time = output.strip().split('|')
-    return ({
-        'hash': commit_hash,
-        'description': description,
-        'time': time
-    })
+    @app.route('/commit')
+    def commit():
+        output = os.popen('git log -1 --pretty=format:"%h|%s|%cr"').read()
+        commit_hash, description, time = output.strip().split('|')
+        return ({
+            'hash': commit_hash,
+            'description': description,
+            'time': time
+        })
 
 
 if __name__ == '__main__':
